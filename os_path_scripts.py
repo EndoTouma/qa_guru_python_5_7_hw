6fcac986a188a0e08752962a1a01d97553de8cfe
Evgenii Vervai <e.vervay@aqsi.ru>
2023-04-26 14:20:03 +0300

    Небольшие исправления

--- os_path_scripts.py
+++ os_path_scripts.py
@@ -5,4 +5,5 @@ CURRENT_FILE_PATH = os.path.abspath(__file__)
 PROJECT_ROOT_PATH = os.path.dirname(CURRENT_FILE_PATH)
 
 RESOURCES_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources', )
-print(RESOURCES_PATH)
+
+TESTS_PATH = os.path.join(PROJECT_ROOT_PATH, 'tests')
