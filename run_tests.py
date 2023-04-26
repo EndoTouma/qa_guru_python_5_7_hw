a91ebfd1aec1f34403c98a0813df166acc378232
Evgenii Vervai <e.vervay@aqsi.ru>
2023-04-26 14:20:03 +0300

    add run_tests and TESTS_PATH to os_path_scripts

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 
+from os_path_scripts import TESTS_PATH
+
 if __name__ == '__main__':
-    test_dir = os.path.join(os.path.dirname(__file__), 'tests')
-    pytest.main(['-v', test_dir])
+	test_dir = os.path.join(TESTS_PATH)
+	pytest.main(['-v', test_dir])
